242793adcdc37baf8fc03dd508e7cdf3aba4c870
Mohammad Outadi <43797071+mroutadi@users.noreply.github.com>
2019-07-13 20:35:13 +0430

    Our JS upto header
    My lovely JS file

---
+++ JS/JS.js
@@ -0,0 +1,15 @@
+// function Toggle() {
+//   var x = document.getElementsById("fortoggle");
+//   if (x.className === "navUL") {
+//     x.className += ".responsive";
+//   } else {
+//     x.className = "navUL"
+//   }
+// }
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}

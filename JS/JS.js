3dfd49c29e94fe8dc333421a02eee73ebadc1697
Mohammad Outadi <43797071+mroutadi@users.noreply.github.com>
2019-07-16 17:26:14 +0430

    Product Page added
    a produc page added that isnt complete but only i add this because of one backup

--- JS/JS.js
+++ JS/JS.js
@@ -1,15 +1,39 @@
-// function Toggle() {
-//   var x = document.getElementsById("fortoggle");
-//   if (x.className === "navUL") {
-//     x.className += ".responsive";
-//   } else {
-//     x.className = "navUL"
-//   }
-// }
+// these are for thae mobile navbar
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+}
+//these are for product slider
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("SLDmySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
